0be344ebf837918f532ff172a845bad20af2d96a
Alexis Quintuna <alexisquintuna@Alexiss-MacBook-Air.local>
2021-08-05 18:13:42 -0400

    backend done?

--- djangostock/stockapp/quote/urls.py
+++ djangostock/stockapp/quote/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('delete_stock/', views.delete_stock, name='delete_stock'),
+    path('add_stock/', views.add_stock, name='add_stock'),
+    path('delete/<stock_id>', views.delete, name='delete'),
 ]
